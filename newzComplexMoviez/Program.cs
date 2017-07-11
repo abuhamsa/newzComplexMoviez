d3914d9607867311e7d023c2b4a33caae5d94f3f
Jan Reber <jan.reber@postfinance.ch>
2017-07-11 13:33:53 +0200

    testing proxy

--- newzComplexMoviez/Program.cs
+++ newzComplexMoviez/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//blubber
 namespace newzComplexMoviez
 {
     static class Program
